88d34338ed65e85d4fe2a3d4581decf1a3344b9b
Ioannis Athanasiadis <ioannis.athanasiadis@siemens.com>
2024-09-15 12:21:50 +0200

    reduce code in roll

--- roll/roll.h
+++ roll/roll.h
@@ -1,13 +1,9 @@
 #ifndef ROLL
 #define ROLL
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 
 void roll_init(void);
 
-int roll(int min, int max);
-int roll_exclusive(int min, int max, int *excl_vavues_list, int excl_vavues_no);
+int roll(const int min, const int max);
+int roll_exclusive(const int min, const int max, const int excl_vavues_list[], const int excl_vavues_no);
 
 #endif //ROLL
