60f3dbabadcfb96fe6f831bccf3265880fa49b68
ioaath22 <ioannis.athanasiadis@siemens.com>
2024-12-27 15:04:31 +0100

    enable OTA with separate file

--- puzzle_game.ino
+++ puzzle_game.ino
@@ -1,13 +1,29 @@
+#define PRINT_FUNCTION Serial.print
 #include "src/abstract_game/puzzle.h"
 #include "src/mui/mui.h"
+#include "src/ota/credentials.h"
 
+#include <WebOTA.h>
+
+
+game_state_t game_state;
+bool initializationCheck = true;
 
 void setup() {
-    // put more setup code here, to run once:
-    puzzleGame(init_gui_structures, delete_gui_structures);
+    Serial.begin(115200);
+    init_wifi(ssid, password, host);
+    initializationCheck = puzzleGameInit(&game_state, init_mui_structures) == SUCCESS;
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:
+  if (initializationCheck)
+  {
+  puzzleGameMainIteration(&game_state);
+  }
+  webota.handle();
+}
 
+void terminate() {
+  puzzleGameTermination(delete_mui_structures);
 }
